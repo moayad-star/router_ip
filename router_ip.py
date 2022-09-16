1be4dfbbefa88e8ccb12e882d00df180d787c1f0
Moayad Ahmed <60769512+moayad-star@users.noreply.github.com>
2022-09-16 20:13:21 +0300

    start file

--- router_ip.py
+++ router_ip.py
@@ -1,18 +1,20 @@
+# Modules
 from os import system as UI
 try:
     import requests
 except (ModuleNotFoundError,FileNotFoundError):
     UI("pip install requests ; clear")
     import requests
+
+# Check your connection status
 try:
     test_connect = requests.get("http://www.google.com")
 except (requests.exceptions.ConnectTimeout,requests.exceptions.ConnectionError,requests.exceptions.ReadTimeout): # Handling errors
     exit("\033[;41;mYou are offline !\033[;40;m")
-num_while = 0
-num_one_point = 0
+
+# start
 try:
-    while num_while != 256:
-            num_while += 1
+    for num_one_point in range(256):
             for num_sec_point in range(256):
                 link = f"http://192.168.{str(num_one_point)}.{str(num_sec_point)}"
                 try:
@@ -21,6 +23,5 @@ try:
                     continue
                 if url == "<Response [200]>":
                     print(f"\033[;37;m[+] Success \033[;32;m  {link}")
-            num_one_point += 1
 except KeyboardInterrupt:
     exit("\nExit...")
